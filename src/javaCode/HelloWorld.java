1e6b2829fefc6d7cf1b5fc6453cfcc87cbbe3f7a
KarthikeyanLearning <Karthiksoft23@gmail.com>
2021-03-05 23:02:09 -0800

    First Commit

---
+++ src/javaCode/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaCode;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("This is 1st line code");
+        System.out.println("This is 2nd line code");
+        System.out.println("This is 3rd line code");
+        System.out.println("This is 4th line code");
+    }
+}
